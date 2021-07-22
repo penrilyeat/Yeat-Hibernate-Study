220bf24f95e2f98fafc205103a5b75679ac5a31f
penrilyeat <87742834+penrilyeat@users.noreply.github.com>
2021-07-22 11:28:22 +0800

    Add files via upload

---
+++ HibernateMappingRelationsOneToMany/src/main/java/com/yeat/HibernateMappingRelationsOneToMany/App.java
@@ -0,0 +1,38 @@
+package com.yeat.HibernateMappingRelationsOneToMany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Laptop laptop=new Laptop();
+    	laptop.setLid(101);
+    	laptop.setLname("Dell");
+    	
+    	Student stu=new Student();
+    	stu.setName("Yeat");
+    	stu.setRollno(1);
+    	stu.setMarks(50);
+    	stu.getLaptop().add(laptop);
+    	laptop.setStudent(stu);
+    	Configuration con=new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+        SessionFactory sf=con.buildSessionFactory();
+        Session s=sf.openSession();
+        s.beginTransaction();
+        s.save(laptop);
+        s.save(stu);
+        
+        s.getTransaction().commit();
+        s.close();
+    }
+}
